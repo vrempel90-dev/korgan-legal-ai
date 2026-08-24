28803b72b369293096dec1999c14f32405d0ff3d
vrempel90-dev <vrempel90@gmail.com>
2026-08-24 16:36:14 +0500

    test: avoid import-time production mutation

--- tests/test_production_runtime_wiring.py
+++ tests/test_production_runtime_wiring.py
@@ -1,7 +1,6 @@
-from korgan.production_runtime import ProductionOpenAILegalService, bot_module
+from korgan.production_runtime import ProductionOpenAILegalService
 from korgan.state_duty_final_hotfix import ProductionOpenAILegalService as FinalLegalService
 
 
-def test_production_runtime_wires_final_guarded_legal_service() -> None:
+def test_production_runtime_selects_final_guarded_legal_service() -> None:
     assert ProductionOpenAILegalService is FinalLegalService
-    assert bot_module.OpenAILegalService is FinalLegalService
