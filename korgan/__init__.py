48381feb7190e7b8ff2c3b905559269de9544f1e
vrempel90-dev <vrempel90@gmail.com>
2026-09-02 23:55:04 +0500

    Install deterministic claim language compatibility guard

--- korgan/__init__.py
+++ korgan/__init__.py
@@ -3,6 +3,7 @@
 from korgan import claim_quality_hotfix as _claim_quality_hotfix
 from korgan.claim_consistency_guard import install_claim_consistency_guard
 from korgan.claim_core_release_runtime import install_claim_core_release_guard
+from korgan.claim_language_compat import install_claim_language_compat
 from korgan.claim_release_repair import install_claim_release_repair
 from korgan.claim_upload_material_bridge import install_claim_upload_material_bridge
 from korgan.client_document_feedback_safe import install_client_document_feedback_safe
@@ -13,6 +14,7 @@ install_claim_release_repair()
 install_claim_upload_material_bridge()
 install_client_document_feedback_safe()
 install_legacy_agent_rag_bridge()
+install_claim_language_compat()
 
 # strict_bot imports install_runtime_hotfix only after package initialization.
 # claim_release_repair has already replaced that installer with its production
